5ae1f5ebff39f9639cc90c842ad21eebc421f4f2
dodying <549779963@qq.com>
2016-08-06 15:14:27 +0800

    Update hvTrainer.user.js

--- HentaiVerse/hvTrainer.user.js
+++ HentaiVerse/hvTrainer.user.js
@@ -1,6 +1,11 @@
 // ==UserScript==
 // @name        hvTrainer
+// @name:zh-CN  【HV】训练
 // @namespace   https://github.com/dodying/Dodying-UserJs
+// @author      Dodying
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @supportURL  https://github.com/dodying/Dodying-UserJs/issues
+// @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
 // @include     http://hentaiverse.org/*
 // @version     1
 // @grant       none
