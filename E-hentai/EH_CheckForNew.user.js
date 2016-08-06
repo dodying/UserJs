9e913dec09b371fae71c2f41a125d6c99c3a9fa8
dodying <549779963@qq.com>
2016-08-06 15:26:08 +0800

    Update EH_CheckForNew.user.js

--- E-hentai/EH_CheckForNew.user.js
+++ E-hentai/EH_CheckForNew.user.js
@@ -1,39 +1,44 @@
 // ==UserScript==
 // @name        EH_CheckForNew
 // @name:zh-CN  【EH】检查新本子
-// @namespace   https://github.com/dodying/Dodying-UserJs
 // @author      Dodying
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @supportURL  https://github.com/dodying/Dodying-UserJs/issues
+// @icon        http://cdn4.iconfinder.com/data/icons/mood-smiles/80/mood-29-48.png
 // @description A monitor for EH
 // @description:zh-CN 详见帮助
-// @include     http://g.e-hentai.org/
-// @include     http://g.e-hentai.org/?*
-// @include     http://g.e-hentai.org/tag/*
-// @include     http://g.e-hentai.org/g/*
-// @include     http://g.e-hentai.org/uploader/*
-// @include     http://g.e-hentai.org/favorites.php*
-// @include     http://exhentai.org/
-// @include     http://exhentai.org/?*
-// @include     http://exhentai.org/tag/*
-// @include     http://exhentai.org/g/*
-// @include     http://exhentai.org/uploader/*
-// @include     http://exhentai.org/favorites.php*
-// @version     1
+// @include     http*://g.e-hentai.org/
+// @include     http*://g.e-hentai.org/?*
+// @include     http*://g.e-hentai.org/tag/*
+// @include     http*://g.e-hentai.org/g/*
+// @include     http*://g.e-hentai.org/uploader/*
+// @include     http*://g.e-hentai.org/favorites.php*
+// @include     http*://exhentai.org/
+// @include     http*://exhentai.org/?*
+// @include     http*://exhentai.org/tag/*
+// @include     http*://exhentai.org/g/*
+// @include     http*://exhentai.org/uploader/*
+// @include     http*://exhentai.org/favorites.php*
+// @version     1.00
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_deleteValue
 // @grant       GM_listValues
-// @icon        http://cdn4.iconfinder.com/data/icons/mood-smiles/80/mood-29-48.png
+// @grant       GM_addStyle
 // @run-at      document-idle
 // ==/UserScript==
+var script = document.createElement('script');
+script.src = 'https://raw.githubusercontent.com/dodying/UserJs/master/Lib/sortTable.js';
+document.head.appendChild(script);
 if (document.querySelector('input.stdinput,form>input[name="favcat"]+div>input').value) {
   document.title = document.querySelector('input.stdinput,form>input[name="favcat"]+div>input').value;
 }
-var EH_CheckForNew_Button = document.createElement('div');
-EH_CheckForNew_Button.innerHTML = '<button id="EH_CheckForNew_Add">保存</button><br /><button id="EH_CheckForNew_All">列表</button>';
-EH_CheckForNew_Button.style = 'position:absolute;left:' + eval(document.documentElement.clientWidth - 110) + 'px;top:0px;z-index:999;';
+var EH_CheckForNew_Button = document.createElement('span');
+EH_CheckForNew_Button.innerHTML = '<img src="http://exhentai.org/img/mr.gif" alt=""><a id="EH_CheckForNew_Add" href="javascript:#"> 保存</a><img src="http://exhentai.org/img/mr.gif" alt=""><a id="EH_CheckForNew_All" href="javascript:#"> 列表</a>';
+//EH_CheckForNew_Button.style = 'position:absolute;left:' + eval(document.documentElement.clientWidth - 150) + 'px;top:0px;z-index:999;';
 EH_CheckForNew_Button.querySelector('#EH_CheckForNew_Add').onclick = function () {
   var Url = window.location.href;
-  var Id = GM_getValue('length', 0) + 1;
+  var Id = getEmpty();
   if (!GM_getValue('FavUrl')) {
     var FavUrl = new Object;
     FavUrl[Url] = 1;
@@ -54,56 +59,87 @@ EH_CheckForNew_Button.querySelector('#EH_CheckForNew_Add').onclick = function ()
     }
   }
   if (document.querySelector('h1#gn')) {
-    var BookName = document.querySelector('h1#gn').innerHTML;
+    var BookName = document.querySelector('h1#gn').innerText.replace(/\[.*?\]|\(.*?\)|【.*?】/g, '').replace(/^\s+|\s+$/g, '');
+    var LastPost = document.querySelector('.itd').innerText;
+    var Keyword = BookName;
   } else {
     var BookName = '';
+    var LastPost = document.querySelector('tr.gtr1:nth-child(3) > td:nth-child(2)').innerText;
+    var Keyword = document.querySelector('input.stdinput,form>input[name="favcat"]+div>input').value;
   }
   var Title = prompt('请输入名称', BookName);
   if (Title == null || Title == '') return false;
+  Title = Title.replace(/\[\d+(\/|\-)\d+(\/|\-)\d+\]/, '');
   GM_setValue('FavUrl', FavUrl);
   var Today = new Date();
   var BookmarkInfo = {
     'Id': Id,
     'Title': Title,
-    'LastTime': Today,
-    'Url': Url
+    'LastUpdateTime': Today,
+    'LastPostTime': LastPost,
+    'Url': Url,
+    'Keyword': decodeURIComponent(Keyword).replace(/\+/g, ' ').replace('parody', '同人').replace(/(.*)language\:chinese\$/, '[中文]$1').replace(/^\s+|\s+$/g, '')
   };
   GM_setValue(Id, BookmarkInfo);
   GM_setValue('FavSort', FavSort);
-  GM_setValue('length', Id);
   window.location = window.location;
 }
 EH_CheckForNew_Button.querySelector('#EH_CheckForNew_All').onclick = function () {
-  if (!document.querySelector('#EH_CheckForNew_Table')) {
-    var FavSort = GM_getValue('FavSort');
-    //console.log(FavSort);
+  if (document.querySelector('#EH_CheckForNew_Table')) {
+    if (document.querySelector('#EH_CheckForNew_Table').style.display === 'none') {
+      document.querySelector('#EH_CheckForNew_Table').style.display = 'block';
+      this.innerHTML = ' 隐藏';
+    } else {
+      document.querySelector('#EH_CheckForNew_Table').style.display = 'none';
+      this.innerHTML = ' 显示';
+    }
+  } else {
+    this.innerHTML = ' 隐藏';
     var bgcolor;
     (window.location.host == 'g.e-hentai.org') ? bgcolor = '#E3E0D1' : bgcolor = '#34353B';
     var EH_CheckForNew_Table = document.createElement('div');
     EH_CheckForNew_Table.id = 'EH_CheckForNew_Table';
-    EH_CheckForNew_Table.style = 'z-index:999;width:700px;background-color:' + bgcolor + ';position:relative;top:-' + eval(document.body.scrollHeight - 270) + 'px;border-color:black;border-style:solid;text-align:center;z-index:999;margin: 2px auto 0px;';
-    EH_CheckForNew_Table.innerHTML = '<button onclick="this.parentNode.style.display=\'none\';">隐藏</button><div style="height:600px;overflow:auto;"><table style="margin: 0px auto;"><tbody><tr><th>ID</th><th width="200">Title</th><th>LastTime</th><th width="200">Url</th><td>更改</td></tr></tbody></table></div>';
+    EH_CheckForNew_Table.style = 'z-index:999;width:640px;background-color:' + bgcolor + ';position:relative;top:-' + eval(document.body.scrollHeight - 270) + 'px;border-color:black;border-style:solid;text-align:center;z-index:999;margin: 2px auto 0px;';
+    EH_CheckForNew_Table.innerHTML = '<table><tbody><tr><td><input type="checkbox"></td><td onclick="sortTable(\'EH_CheckForNew_Table_Content\',1,\'int\');">ID</td><td onclick="sortTable(\'EH_CheckForNew_Table_Content\',2,\'date\');">LastUpdateTime</td><td onclick="sortTable(\'EH_CheckForNew_Table_Content\',3,\'date\');">LastPostTime</td><td onclick="sortTable(\'EH_CheckForNew_Table_Content\',4);">Url</td><td onclick="sortTable(\'EH_CheckForNew_Table_Content\',5);">Keyword</td></tr></tbody></table><div style="height:600px;overflow:auto;"><table id="EH_CheckForNew_Table_Content"><tbody></tbody></table></div><button class="stdbtn" id="EH_CheckForNew_Batch_Update">更新</button><button class="stdbtn" id="EH_CheckForNew_Batch_Delete">删除</button>';
+    var FavSort = GM_getValue('FavSort', [
+    ]);
     for (var i = FavSort.length - 1; i >= 0; i--) {
       //for (var i = 0; i < FavSort.length; i++) {
       var temp = GM_getValue(FavSort[i]);
       var tr = document.createElement('tr');
-      tr.innerHTML = '<td>' + temp['Id'] + '</td><td>' + temp['Title'] + '</td><td>' + temp['LastTime'] + '</td><td><a target="_blank" href="' + temp['Url'] + '">' + temp['Title'] + '</a></td><td><button class="EH_CheckForNew_Class_Update">更新</button><button class="EH_CheckForNew_Class_Delete">删除</button><td>';
-      tr.querySelector('.EH_CheckForNew_Class_Update').onmousedown = function (event) {
-        var Id = this.parentNode.parentNode.firstChild.innerHTML;
-        UpdateBookmark(Id, event.button, this);
+      tr.innerHTML = '<td><input type="checkbox"></td><td>' + temp['Id'] + '</td><td>' + temp['LastUpdateTime'].substr(0, 16).replace('T', ' ') + '</td><td>' + temp['LastPostTime'] + '</td><td><a target="_blank" href="' + temp['Url'] + '">' + temp['Title'] + '</a></td><td>' + temp['Keyword'] + '</td>';
+      EH_CheckForNew_Table.querySelector('#EH_CheckForNew_Table_Content>tbody').appendChild(tr);
+    }
+    EH_CheckForNew_Table.querySelector('#EH_CheckForNew_Batch_Update').onmousedown = function (e) {
+      var input = EH_CheckForNew_Table.querySelectorAll('input:checked');
+      var Id_arr = new Array();
+      for (var i = 0; i < input.length; i++) {
+        Id_arr.push(input[i].parentNode.nextElementSibling.innerText);
       }
-      tr.querySelector('.EH_CheckForNew_Class_Delete').onclick = function () {
-        var Id = this.parentNode.parentNode.firstChild.innerHTML;
-        DeleteBookmark(Id)
+      for (var i = 0; i < Id_arr.length; i++) {
+        Id = Id_arr[i];
+        UpdateBookmark(Id, false, e.button);
       }
-      EH_CheckForNew_Table.querySelector('div>table>tbody').appendChild(tr);
+      window.location = window.location;
+    }
+    EH_CheckForNew_Table.querySelector('#EH_CheckForNew_Batch_Delete').onclick = function () {
+      var input = EH_CheckForNew_Table.querySelectorAll('input:checked');
+      var Id_arr = new Array();
+      for (var i = 0; i < input.length; i++) {
+        Id_arr.push(input[i].parentNode.nextElementSibling.innerText);
+      }
+      for (var i = 0; i < Id_arr.length; i++) {
+        Id = Id_arr[i];
+        DeleteBookmark(Id, false);
+      }
+      window.location = window.location;
     }
     document.body.appendChild(EH_CheckForNew_Table);
-  } else {
-    document.querySelector('#EH_CheckForNew_Table').style.display = 'block';
+    GM_addStyle('#EH_CheckForNew_Table table{margin: 0px auto;}#EH_CheckForNew_Table td:nth-child(1){width:16px !important;}#EH_CheckForNew_Table td:nth-child(2){width:24px !important;}#EH_CheckForNew_Table td:nth-child(3),#EH_CheckForNew_Table td:nth-child(4){width:90px !important;}#EH_CheckForNew_Table td:nth-child(5),#EH_CheckForNew_Table td:nth-child(6){width:200px !important;}')
   }
 }
-document.body.appendChild(EH_CheckForNew_Button);
+document.all.nb.appendChild(EH_CheckForNew_Button);
+document.all.nb.style.height = '16px';
 /**/
 var FavUrl = GM_getValue('FavUrl');
 if (FavUrl) {
@@ -113,28 +149,30 @@ if (FavUrl) {
     var FavInfo = GM_getValue(Id);
     //console.log(FavInfo);
     var Title = FavInfo['Title'];
-    var Today_Local = new Date().toLocaleDateString();
-    document.title = '[' + Today_Local + ']' + Title;
+    var TodayUTC = getNowFormatDate();
+    document.title = '[' + TodayUTC + ']' + Title;
     var time = document.querySelectorAll('table.itg>tbody>tr>td:nth-child(2)');
     for (var i = 0; i < time.length; i++) {
-      var PostTime = Date.parse(time[i].innerHTML.replace(/ /, 'T'));
-      if (PostTime < Date.parse(FavInfo['LastTime']))
+      var PostTime = Date.parse(time[i].innerText.replace(/ /, 'T'));
+      if (PostTime < Date.parse(FavInfo['LastUpdateTime']))
       break;
     }
     var tr = document.createElement('tr');
+    tr.id = 'EH_CheckForNew';
     tr.style = 'text-align:center;';
-    tr.innerHTML = '<td colspan="4"><div>上次检查时间为' + FavInfo['LastTime'] + '<button id="EH_CheckForNew_Update">更新</button><button id="EH_CheckForNew_Delete">删除</button><input onclick="this.select();" value="[' + Today_Local + ']' + Title + '" /></div></td>';
-    tr.querySelector('#EH_CheckForNew_Update').onmousedown = function (event) {
-      UpdateBookmark(Id, event.button, this);
+    tr.innerHTML = '<td colspan="4"><div>上次检查时间为 ' + FavInfo['LastUpdateTime'].substr(0, 16).replace('T', ' ') + ' <button id="EH_CheckForNew_Update" class="stdbtn">更新</button><button id="EH_CheckForNew_Delete" class="stdbtn">删除</button><input onclick="this.select();" value="[' + TodayUTC + ']' + Title + '" /></div></td>';
+    tr.querySelector('#EH_CheckForNew_Update').onmousedown = function (e) {
+      UpdateBookmark(Id, true, e.button);
     }
     tr.querySelector('#EH_CheckForNew_Delete').onclick = function () {
-      DeleteBookmark(Id);
+      DeleteBookmark(Id, true);
     }
     document.querySelector('table.itg>tbody').insertBefore(tr, document.querySelector('table.itg>tbody>tr:nth-child(' + eval(i + 2) + ')'));
   }
 } /**/
+//////////////////////////////////////////////////
 
-function DeleteBookmark(Id) {
+function DeleteBookmark(Id, reload) {
   var Url = GM_getValue(Id).Url;
   GM_deleteValue(Id);
   var FavUrl = GM_getValue('FavUrl');
@@ -148,9 +186,10 @@ function DeleteBookmark(Id) {
     }
   }
   GM_setValue('FavSort', FavSort);
-  window.location = window.location;
-}
-function UpdateBookmark(Id, status, ele) {
+  if (reload) window.location = window.location;
+} //////////////////////////////////////////////////
+
+function UpdateBookmark(Id, reload, status) {
   var FavInfo = GM_getValue(Id);
   switch (status) {
     case 0:
@@ -160,15 +199,16 @@ function UpdateBookmark(Id, status, ele) {
       var Title = prompt('请输入名称', FavInfo['Title']);
       break;
   }
-  var Url = FavInfo['Url'];
-  if (ele.parentNode.querySelector('input'))
-  ele.parentNode.querySelector('input').value = '[' + Today_Local + ']' + Title;
-  var Today = new Date();
+  var Now = new Date();
+  var LastPost = document.querySelector('.itd').innerText;
+  var Id = FavInfo['Id'];
   var BookmarkInfo = {
     'Id': Id,
     'Title': Title,
-    'LastTime': Today,
-    'Url': Url
+    'LastUpdateTime': Now,
+    'LastPostTime': LastPost,
+    'Url': FavInfo['Url'],
+    'Keyword': FavInfo['Keyword']
 };
 //console.log(BookmarkInfo);
 GM_setValue(Id, BookmarkInfo);
@@ -181,5 +221,36 @@ for (var i = 0; i < FavSort.length; i++) {
 }
 FavSort.unshift(Id);
 GM_setValue('FavSort', FavSort);
-window.location = window.location;
+if (reload) window.location = window.location;
+} //////////////////////////////////////////////////
+
+function getNowFormatDate() {
+var date = new Date();
+var month = date.getUTCMonth() + 1;
+var strDate = date.getUTCDate();
+if (month >= 1 && month <= 9) {
+month = '0' + month;
+}
+if (strDate >= 0 && strDate <= 9) {
+strDate = '0' + strDate;
+}
+var currentdate = date.getFullYear() + '-' + month + '-' + strDate;
+return currentdate;
+} //////////////////////////////////////////////////
+
+function getEmpty() {
+var arr = GM_getValue('FavSort', 0);
+if (arr === 0) return 1;
+arr.sort(sortNumber);
+for (var i = 0; i < arr.length; i++) {
+if (i + 1 !== arr[i]) {
+  var num = i + 1;
+  break;
+}
+var num = i + 2;
+}
+return num;
+function sortNumber(a, b) {
+return a - b
+}
 }
