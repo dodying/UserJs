d6fecd048e1b47aaeb8a322886da1e312ff45773
dodying <549779963@qq.com>
2016-08-06 17:25:27 +0800

    Update hvAutoAttack.user.js

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -1,12 +1,3 @@
-/*
- * 这是一张 JavaScript 代码草稿纸。
- *
- * 输入一些 JavaScript，然后可点击右键或从“执行”菜单中选择：
- * 1. 运行 对选中的文本求值(eval) (Ctrl+R)；
- * 2. 查看 对返回值使用对象查看器 (Ctrl+I)；
- * 3. 显示 在选中内容后面以注释的形式插入返回的结果。 (Ctrl+L)
- */
-
 // ==UserScript==
 // @name        hvAutoAttack
 // @name:zh-CN  【HV】打怪
