14d0a3f21aa5abb5e2b2ca8146ad0abcf1243182
dodying <549779963@qq.com>
2016-08-06 14:58:59 +0800

    Create baiduShareCopy.user.js

---
+++ pan.baidu.com/baiduShareCopy.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        baiduShareCopy
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @name:zh-CN  
+// @description:zh-CN  
+// @include     http://pan.baidu.com/share/manage
+// @version     1
+// @grant       GM_setClipboard
+// @author      Dodying
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @supportURL  https://github.com/dodying/Dodying-UserJs/issues
+// @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
+// @require     http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js
+// @run-at      document-end
+// ==/UserScript==
+var interval = setInterval(function () {
+  if (jQuery('div.btns:has(a.unshare)').length === 0) {
+    return;
+  } else {
+    clearInterval(interval);
+    jQuery('div.btns:has(a.unshare)').append('<a class="btn copy"></a>');
+    jQuery('head').append('<style>.copy{background-position:-160px -106px !important;cursor:pointer;}.copy:hover{background-position:-183px -106px !important;}</style>');
+    jQuery('.copy').click(function () {
+      var txt = jQuery(this).parents('.item').find('.copy-bar').text().replace(/\s+/g, ' ').replace('复制', '').replace(/^\s+|\s+$/g, '');
+      GM_setClipboard(txt);
+    });
+  }
+}, 200);
