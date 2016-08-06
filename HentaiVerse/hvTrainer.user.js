77a476a164061721c4b3c749a53ce576e766f24a
dodying <549779963@qq.com>
2016-08-06 15:12:06 +0800

    Update and rename Trainer.user.js to hvTrainer.user.js

--- HentaiVerse/hvTrainer.user.js
+++ HentaiVerse/hvTrainer.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Trainer
+// @name        hvTrainer
 // @namespace   https://github.com/dodying/Dodying-UserJs
 // @include     http://hentaiverse.org/*
 // @version     1
