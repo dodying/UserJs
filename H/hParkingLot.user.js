f94af7e536c4df50becb98200f98dfac0172363d
dodying <549779963@qq.com>
2016-10-17 14:09:54 +0800

    Update hParkingLot.user.js

--- H/hParkingLot.user.js
+++ H/hParkingLot.user.js
@@ -3,6 +3,7 @@
 // @name:zh-CN  【H】停车场
 // @namespace   https://github.com/dodying/Dodying-UserJs
 // @include     https://btso.pw/*
+// @include     https://btdb.in/*
 // @include     http://www.javlibrary.com/*
 // @include     http://www.dmm.co.jp/*
 // @include     https://www.google.co.jp/*
@@ -14,7 +15,7 @@
 // include     *.10musume.com/*
 // include     *.heyzo.com/*
 // include     *.1pondo.tv/*
-// @version     1.01
+// @version     1.02
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @author      Dodying
@@ -28,43 +29,48 @@
 (function ($) {
   var linkLib = {
     'btso.pw': {
-      url: 'http://btso.pw/search/{searchTerms}',
       fav: 'https://btso.pw/app/bts/View/img/favicon.ico',
-      text: 'h3,.file',
-      img: ''
+      search: 'http://btso.pw/search/{searchTerms}/',
+      text: 'h3,.file'
+    },
+    'btdb.in': {
+      fav: 'https://btdb.in/favicon.ico',
+      search: 'https://btdb.in/q/{searchTerms}/',
+      text: 'h1.torrent-name,.file-name,.item-title>a'
     },
     'www.javlibrary.com': {
-      url: 'http://www.javlibrary.com/cn/vl_searchbyid.php?keyword={searchTerms}',
       fav: 'http://www.javlibrary.com/favicon.ico',
-      name: '.post-title',
-      time: '.text:eq(2)',
+      search: 'http://www.javlibrary.com/cn/vl_searchbyid.php?keyword={searchTerms}',
       text: '.text:lt(2),.id',
-      img: '#video_jacket_img,.previewthumbs>img,.id+img,strong>a'
+      img: '#video_jacket_img,.previewthumbs>img,.id+img,strong>a',
+      name: '.post-title',
+      time: '.text:eq(2)'
     },
     'www.dmm.co.jp': {
-      url: 'http://www.dmm.co.jp/search/=/searchstr={searchTerms}',
       fav: 'http://www.dmm.co.jp/favicon.ico',
-      name: '#title',
+      search: 'http://www.dmm.co.jp/search/=/searchstr={searchTerms}',
       text: '.txt,table.mg-b20 td',
-      img: '.img img,.tdmm,.crs_full>img'
+      img: '.img img,.tdmm,.crs_full>img',
+      name: '#title',
+      time: ''
     },
     'www.google.co.jp': {
-      url: 'https://www.google.co.jp/search?q={searchTerms}',
       fav: 'https://www.google.co.jp/images/branding/product/ico/googleg_lodp.ico',
-      text: 'h3.r>a,span.st',
-      img: ''
+      search: 'https://www.google.co.jp/search?q={searchTerms}',
+      text: 'h3.r>a,span.st'
     },
     'www.baidu.com': {
-      url: 'https://www.baidu.com/baidu?wd={searchTerms}',
       fav: 'https://www.baidu.com/img/baidu.svg',
-      text: 'h3.t>a,.c-abstract',
-      img: ''
+      search: 'https://www.baidu.com/baidu?wd={searchTerms}',
+      text: 'h3.t>a,.c-abstract'
     },
     'www.av28.com': {
-      url: 'https://www.av28.com/cn/search/{searchTerms}',
       fav: 'https://cdn3.iconfinder.com/data/icons/block/32/letter-24.png',
+      search: 'https://www.av28.com/cn/search/{searchTerms}',
       text: '.item>div>a,.item>div>span,.row-fluid>h3,.info>p>span',
-      img: '.img,.bigImage>img,.sample-box img'
+      img: '.img,.bigImage>img,.sample-box img',
+      name: '',
+      time: ''
     }
   };
   var imgLib = {
@@ -100,6 +106,11 @@
       color: 'black'
     },
     {
+      name: '女同',
+      img: 'https://cdn2.iconfinder.com/data/icons/gender-and-feminism-solid/100/lesbian-24.png',
+      color: 'pink'
+    },
+    {
       name: '超过1年无资源',
       img: 'https://cdn3.iconfinder.com/data/icons/transfers/100/239345-reload_time_delay-24.png',
       color: 'red'
@@ -114,15 +125,17 @@
     '.right{right:0;}' +
     '.top{top:0;}' +
     '.bottom{bottom:0;}' +
-    '.hBanner>*{cursor:pointer;float:left;margin:0 5px;}' +
+    '.hBanner>*{cursor:pointer;float:left;margin:0 1px;}' +
     '.switcher{width:32px;height:24px;background:#333;border-radius:12px;position:relative;}' +
     '.switcher>span{position:absolute;left:6px;top:2px;height:2px;color:#26CA28;font-size:16px;text-transform:Capitalize;}' +
     '.links,.addCode,.delCode,.importCode,.showCode{width:24px;height:24px;}' +
     '.links img,.addCode img{background-color:white;}' +
     '.links>*,.addCode>*{display:none;}' +
     '.links>*:nth-child(1),.addCode>*:nth-child(1){display:inline;}' +
+    '.hasCode>a{margin:0 1px;}' +
     '.showTable{background-color:white;position:fixed;top:40px;height:400px;overflow:auto;left:10px;}');
-    $('<div class="hBanner right top"></div>').dblclick(function () {
+    $('<div class="hBanner right top"></div>').contextmenu(function (e) {
+      e.preventDefault();
       if ($(this).is('.left.top') || $(this).is('.right.bottom')) {
         $(this).toggleClass('left').toggleClass('right');
       } else if ($(this).is('.right.top') || $(this).is('.left.bottom')) {
@@ -141,11 +154,13 @@
     $('<div class="links"></div>').html(function () {
       var _html = '';
       for (var i in linkLib) {
-        _html += '<span onclick="window.open(\'' + linkLib[i].url + '\'.replace(\'{searchTerms}\',this.title))"title="' + getCode() + '"><img src="' + linkLib[i].fav + '" width=24></img></a></span>';
+        _html += '<span onclick="window.open(\'' + linkLib[i].search + '\'.replace(\'{searchTerms}\',this.title))"title="' + getCode() + '"><img src="' + linkLib[i].fav + '" width=24></img></a></span>';
       }
       return _html;
     }).appendTo('.hBanner');
-    $('<div class="addCode"title="添加到数据库/移动"></div>').html('<img src="' + imgLib.add + '"></img>').appendTo('.hBanner');
+    $('<div class="addCode"title="添加到数据库/移动"></div>').html('<img src="' + imgLib.add + '"></img>').click(function () {
+      addValue(GM_getValue('lastMark', 0));
+    }).appendTo('.hBanner');
     $('<div class="delCode"title="从数据库中删除"></div>').html('<img src="' + imgLib.del + '"></img>').click(function () {
       delValue();
     }).appendTo('.hBanner');
@@ -158,8 +173,7 @@
         $('.showTable').toggle();
       });
     }).appendTo('.hBanner');
-    $('<div class="hasCodeImg"></div>').appendTo('.hBanner');
-    $('<div class="hasCodeText"></div>').appendTo('.hBanner');
+    $('<div class="hasCode">(已标记)</div>').appendTo('.hBanner');
     for (var i = 0; i < markLib.length; i++) {
       $('<img src="' + markLib[i].img + '"title="' + markLib[i].name + '"></img>').val(i).click(function () {
         addValue($(this).val());
@@ -175,31 +189,31 @@
     })
   }
   function markAdded() {
+    $('.hasCode a').remove();
     var lib = GM_getValue('lib', null);
     if (!lib) return;
-    $(linkLib[location.host].img).removeAttr('onerror').attr({
-      realSrc: function () {
-        return $(this).attr('src');
-      },
-      src: function () {
-        var output = '(图)：';
-        var keyword;
-        var keyword2;
-        var _src = $(this).attr('src');
-        for (var i in lib) {
-          keyword = new RegExp(i, 'gi');
-          keyword2 = new RegExp(i.replace('-', ''), 'gi');
-          if (keyword.test(_src) || keyword2.test(_src)) {
-            output += '<a href="' + linkLib['www.javlibrary.com'].url + i + '"target="_blank">' + i + '</a> ';
-            _src = markLib[lib[i].mark].img;
+    if (linkLib[location.host].img) {
+      $(linkLib[location.host].img).removeAttr('onerror').attr({
+        realSrc: function () {
+          return $(this).attr('src');
+        },
+        src: function () {
+          var keyword;
+          var keyword2;
+          var _src = $(this).attr('src');
+          for (var i in lib) {
+            keyword = new RegExp(i, 'gi');
+            keyword2 = new RegExp(i.replace('-', ''), 'gi');
+            if (keyword.test(_src) || keyword2.test(_src)) {
+              if ($('.' + i).length === 0) $('<a target="_blank"></a>').addClass(i).attr('href', linkLib['www.javlibrary.com'].search.replace('{searchTerms}', i)).html(i).appendTo('.hasCode');
+              _src = markLib[lib[i].mark].img;
+            }
           }
+          return _src;
         }
-        $('.hasCodeImg').html(output);
-        return _src;
-      }
-    });
+      });
+    }
     $(linkLib[location.host].text).html(function () {
-      var output = '(文)：';
       var keyword;
       var keyword2;
       var _html = $(this).text();
@@ -207,27 +221,29 @@
         keyword = new RegExp(i, 'gi');
         keyword2 = new RegExp(i.replace('-', ''), 'gi');
         if (keyword.test(_html) || keyword2.test(_html)) {
-          output += '<a href="' + linkLib['www.javlibrary.com'].url + i + '"target="_blank">' + i + '</a> ';
+          if ($('.' + i).length === 0) $('<a target="_blank"></a>').addClass(i).attr('href', linkLib['www.javlibrary.com'].search.replace('{searchTerms}', i)).html(i).appendTo('.hasCode');
           _html = _html.replace(keyword, '<span style="background-color:' + markLib[lib[i].mark].color + ';">' + i + '</span>').replace(keyword2, '<span style="background-color:' + markLib[lib[i].mark].color + ';">' + i + '</span>');
         }
       }
-      $('.hasCodeText').html(output);
       return _html;
     });
   }
   function undoMarkAdded() {
     var lib = GM_getValue('lib', null);
     if (!lib) return;
-    $(linkLib[location.host].img).attr({
-      src: function () {
-        return $(this).attr('realSrc');
-      }
-    }).removeAttr('realSrc');
+    if (linkLib[location.host].img) {
+      $(linkLib[location.host].img).attr({
+        src: function () {
+          return $(this).attr('realSrc');
+        }
+      }).removeAttr('realSrc');
+    }
   }
   function addValue(mark, code) { //可选参数code
     var lib = GM_getValue('lib', null) || new Object();
     var code = code || getCode();
     if (!code) return;
+    GM_setValue('lastMark', mark);
     lib[code] = {
       mark: mark,
       name: $(linkLib[location.host].text).text(),
@@ -272,7 +288,7 @@
     var lib = GM_getValue('lib', null) || new Object();
     var _html = '<table class="tablesorter"><thead><tr><th>数字</th><th>标记</th><th>代码</th><th>标题</th><th>时间</th></tr></thead><tbody>';
     for (var i in lib) {
-      _html += '<tr><td>' + lib[i].mark + '</td><td><img src="' + markLib[lib[i].mark].img + '"></img>' + markLib[lib[i].mark].name + '</td><td><a href="' + linkLib['www.javlibrary.com'].url + i + '"target="_blank">' + i + '</a></td><td>' + (lib[i].name || '') + '</td><td>' + (lib[i].time || '') + '</td></tr>';
+      _html += '<tr><td>' + lib[i].mark + '</td><td><img src="' + markLib[lib[i].mark].img + '"></img>' + markLib[lib[i].mark].name + '</td><td><a href="' + linkLib['www.javlibrary.com'].search.replace('{searchTerms}', i) + '"target="_blank">' + i + '</a></td><td>' + (lib[i].name || '') + '</td><td>' + (lib[i].time || '') + '</td></tr>';
     }
     _html += '</tbody></table>';
     $('<div class="showTable"></div>').html(_html).appendTo('body');
