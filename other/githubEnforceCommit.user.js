3cb8c62b9c38fa4fbf273c6930c4f5b83db0d3a5
dodying <549779963@qq.com>
2016-10-23 15:55:28 +0800

    Add files via upload

---
+++ other/githubEnforceCommit.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        githubEnforceCommit
+// @name:zh-CN  【Github】强制Commit
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @description:zh-CN  养成好习惯
+// @include     https://github.com/*/edit/*
+// @version     1
+// @grant       none
+// @author      Dodying
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @supportURL  https://github.com/dodying/Dodying-UserJs/issues
+// @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
+// @run-at      document-end
+// ==/UserScript==
+document.getElementById('submit-file').onclick = function () {
+  if (document.getElementById('commit-summary-input').value === '') return false;
+}
