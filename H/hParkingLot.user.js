97aee4ea1971f6b779b21a66f3d88e16d4f362f0
dodying <549779963@qq.com>
2016-10-24 20:50:25 +0800

    v1.06

--- H/hParkingLot.user.js
+++ H/hParkingLot.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
-// @name        hParkingLot
-// @name:zh-CN  【H】停车场
+// @name        hParkingLot-Rider
+// @name:zh-CN  【H】停车场-骑兵版
 // @namespace   https://github.com/dodying/Dodying-UserJs
+// @description 此为骑兵版，步兵版等待推出
 // @include     https://btso.pw/*
 // @include     https://btdb.in/*
 // @include     http://www.javlibrary.com/*
@@ -10,13 +11,14 @@
 // @include     https://www.baidu.com/baidu?wd=*&status=h
 // @include     https://www.av28.com/*
 // @include     http://javpop.com/*
+// @include     http://www.abase.me/*
 // include     *.tokyo-hot.com/*
 // include     *.caribbeancom.com/*
 // include     *.1000giri.net/*
 // include     *.10musume.com/*
 // include     *.heyzo.com/*
 // include     *.1pondo.tv/*
-// @version     1.05
+// @version     1.06
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @author      Dodying
@@ -29,53 +31,93 @@
 // ==/UserScript==
 (function ($) {
   var linkLib = {
-    'btso.pw': {
-      fav: 'https://btso.pw/app/bts/View/img/favicon.ico',
-      search: 'http://btso.pw/search/{searchTerms}/',
-      text: 'h3,.file'
+    /*
+    'example.com': {
+      cn: 标识,
+      fav: 网站，图标地址,
+      search: 网站，搜索地址，搜索字样用{searchTerms}代替,
+      text: 选择器-要标记的文本,
+      img: 选择器-要标记的图片,
+      time: 选择器-发布日期,
+      code: 选择器-番号
     },
+    */
     'btdb.in': {
+      name: 'BTDB',
       fav: 'https://btdb.in/favicon.ico',
       search: 'https://btdb.in/q/{searchTerms}/',
-      text: 'h1.torrent-name,.file-name,.item-title>a'
+      text: 'h1.torrent-name,.file-name,.item-title>a',
+      code: '#search-input'
+    },
+    'btso.pw': {
+      name: 'BTSOW',
+      fav: 'https://btso.pw/app/bts/View/img/favicon.ico',
+      search: 'http://btso.pw/search/{searchTerms}/',
+      text: 'h3,.file',
+      code: '.form-control:visible'
     },
     'www.javlibrary.com': {
+      name: 'JAVLibrary',
       fav: 'http://www.javlibrary.com/favicon.ico',
       search: 'http://www.javlibrary.com/cn/vl_searchbyid.php?keyword={searchTerms}',
       text: '.text:lt(2),.id,.video>a:not(:has(img))',
       img: '#video_jacket_img,.previewthumbs>img,.id+img,strong>a',
-      time: '.text:eq(2)'
+      time: '.text:eq(2)',
+      code: '#video_id .text'
     },
     'www.dmm.co.jp': {
+      name: 'DMM',
       fav: 'http://www.dmm.co.jp/favicon.ico',
       search: 'http://www.dmm.co.jp/search/=/searchstr={searchTerms}',
       text: '.txt,table.mg-b20 td',
       img: '.img img,.tdmm,.crs_full>img',
-      time: ''
+      time: '.nw:contains(発売日)+td',
+      code: function () {
+        return prompt('请输入番号', $('.nw:contains(品番)+td').text());
+      }
     },
     'www.google.co.jp': {
+      name: 'Google',
       fav: 'https://www.google.co.jp/images/branding/product/ico/googleg_lodp.ico',
       search: 'https://www.google.co.jp/search?q={searchTerms}&status=h',
-      text: 'h3.r>a,span.st'
+      text: 'h3.r>a,span.st',
+      code: '#lst-ib'
     },
     'www.baidu.com': {
+      name: 'Baidu',
       fav: 'https://www.baidu.com/img/baidu.svg',
       search: 'https://www.baidu.com/baidu?wd={searchTerms}&status=h',
-      text: 'h3.t>a,.c-abstract'
+      text: 'h3.t>a,.c-abstract',
+      code: '#kw'
     },
     'www.av28.com': {
+      name: 'AV28',
       fav: 'https://cdn3.iconfinder.com/data/icons/block/32/letter-24.png',
       search: 'https://www.av28.com/cn/search/{searchTerms}',
       text: '.item>div>a,.item>div>span,.row-fluid>h3,.info>p>span',
       img: '.img,.bigImage>img,.sample-box img',
-      time: ''
+      time: '.info>p:eq(1)',
+      code: '.info>p>.header+span'
     },
     'javpop.com': {
+      name: 'JavPOP',
       fav: 'http://javpop.com/favicon.ico',
       search: 'http://javpop.com/index.php?s={searchTerms}',
       text: '.thumb_post a:nth-child(2),h1',
-      img: '.thumb_post img,.box-b img'
-    }
+      img: '.thumb_post img,.box-b img',
+      code: function () {
+        return $('h1').text().replace(/\[(.*?)\].*/, '$1');
+      }
+    },
+    'www.abase.me': {
+      name: 'ABase A基地',
+      fav: 'http://www.abase.me/favicon.ico',
+      search: 'http://www.abase.me/{searchTerms}',
+      text: '.row-fluid>h3,.col-md-3 strong:eq(0),.list-group-item>a',
+      img: '.col-md-9 img',
+      time: '.col-md-3 strong:eq(1)',
+      code: '.col-md-3 strong:eq(0)'
+    },
   };
   var imgLib = {
     add: 'https://cdn2.iconfinder.com/data/icons/freecns-cumulus/16/519691-199_CircledPlus-24.png',
@@ -84,48 +126,48 @@
     show: 'https://cdn2.iconfinder.com/data/icons/freecns-cumulus/16/519904-098_Spreadsheet-24.png'
   };
   var markLib = [
-    {
+    { //0
       name: '等待中',
       img: 'https://cdn2.iconfinder.com/data/icons/lightly-icons/24/time-24.png',
       color: 'gray'
     },
-    {
+    { //1
       name: '下载中',
       img: 'https://cdn4.iconfinder.com/data/icons/education-bold-line-1/49/34-24.png',
       color: 'blue'
     },
-    {
+    { //2
       name: '已出已下-骑兵',
       img: 'https://cdn3.iconfinder.com/data/icons/chess-8/512/horse-game-role-chess-24.png',
       color: 'green'
     },
-    {
+    { //3
       name: '已出已下-步兵',
       img: 'https://cdn3.iconfinder.com/data/icons/chess-8/154/chess-pawn-24.png',
       color: 'green'
     },
-    {
+    { //4
       name: '已出已下已删-不喜欢的',
       img: 'https://cdn1.iconfinder.com/data/icons/lightly-icons/24/heart-broken-24.png',
       color: 'black'
     },
-    {
+    { //5
       name: '女同',
       img: 'https://cdn2.iconfinder.com/data/icons/gender-and-feminism-solid/100/lesbian-24.png',
       color: 'pink'
     },
-    {
+    { //6
       name: '超过1年无资源',
       img: 'https://cdn3.iconfinder.com/data/icons/transfers/100/239345-reload_time_delay-24.png',
       color: 'red'
     },
-    {
-      name: '首次亮相',
+    { //7
+      name: '无H-首次亮相',
       img: 'https://cdn3.iconfinder.com/data/icons/social-messaging-productivity-5/128/new-label-24.png',
       color: 'yellow'
     },
-    {
-      name: '无配额',
+    { //8
+      name: '有种子无配额',
       img: 'https://cdn3.iconfinder.com/data/icons/math-physics/512/null-24.png',
       color: 'gray'
     }
@@ -134,7 +176,7 @@
   markAdded();
   function init() {
     $('<style></style>').appendTo('head').html('' +
-    '.hBanner{position:fixed;background-color:white;z-index:9999;}' +
+    '.hBanner{position:fixed;background-color:white;z-index:999999;}' +
     '.hBanner{-moz-user-select:none;-webkit-user-select:none;-ms-user-select:none;}' +
     '.hBanner>*{cursor:pointer;float:left;margin:0 1px0 1px;}' +
     '.switcher{width:32px;height:24px;background:#333;border-radius:12px;position:relative;}' +
@@ -203,9 +245,12 @@
     $('<div class="links"></div>').html(function () {
       var _html = '';
       for (var i in linkLib) {
-        _html += '<span onclick="window.open(\'' + linkLib[i].search + '\'.replace(\'{searchTerms}\',this.title))"title="' + getCode() + '"><img src="' + linkLib[i].fav + '" width=24></img></a></span>';
+        _html += '<img src="' + linkLib[i].fav + '" width=24 url="' + linkLib[i].search + '"title="' + linkLib[i].name + '"></img>';
       }
       return _html;
+    }).click(function (e) {
+      var code = getCode();
+      window.open($(e.target).attr('url').replace('{searchTerms}', code));
     }).appendTo('.hBanner');
     $('<div class="addCode"title="添加到数据库/移动"></div>').html('<img src="' + imgLib.add + '"></img>').click(function () {
       addValue(GM_getValue('lastMark', 0));
@@ -292,14 +337,15 @@
     }
   }
   function addValue(mark, code) { //可选参数code
+    mark = parseInt(mark);
     var lib = GM_getValue('lib', null) || new Object();
     var code = code || getCode();
     if (!code) return;
     GM_setValue('lastMark', mark);
     lib[code] = {
-      mark: mark,
-      time: $(linkLib[location.host].time).text()
+      mark: mark
     };
+    if (mark === 0 || mark === 6) lib[code].time = $(linkLib[location.host].time).text();
     GM_setValue('lib', lib);
     undoMarkAdded();
     markAdded();
@@ -354,24 +400,16 @@
   }
   function getCode() {
     if ($(window).data('code')) return $(window).data('code');
-    var url = location.href;
-    switch (location.host) {
-      case 'www.dmm.co.jp':
-        var temp = (url.match(/[a-zA-Z]+(\-|)[0-9]+/)) ? url.toUpperCase().match(/[a-zA-Z]+(\-|)[0-9]+/) [0] : url;
-        if (url === temp) return;
-        var code = prompt('请输入车牌号', temp);
-        $(window).data('code', code);
-        return code;
-        break;
-      case 'www.javlibrary.com':
-        return $('#video_id .text').text();
-        break;
-      case 'btso.pw':
-        return url.replace(/.*search\//, '');
-        break;
-      default:
-        return '';
-        break;
+    var code;
+    if (typeof linkLib[location.host].code === 'string') {
+      var temp = $(linkLib[location.host].code);
+      code = ((temp[0].tagName === 'INPUT')) ? temp.val()  : temp.text();
+    } else if (typeof linkLib[location.host].code === 'function') {
+      code = linkLib[location.host].code();
+    } else {
+      code = '';
     }
+    $(window).data('code', code);
+    return code;
   }
 }) (jQuery);
