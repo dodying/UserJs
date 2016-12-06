ed5ff00b4bacb76e800a5276e661d93138b826f7
dodying <dodying@users.noreply.github.com>
2016-12-06 22:18:33 +0800

    v1.01
    不再使用jQuery库，小脚本用什么jQuery啊

--- HentaiVerse/hvTrainer.user.js
+++ HentaiVerse/hvTrainer.user.js
@@ -7,54 +7,59 @@
 // @supportURL  https://github.com/dodying/Dodying-UserJs/issues
 // @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
 // @include     http://hentaiverse.org/*
-// @version     1
+// @version     1.01
 // @grant       none
-// @require     http://libs.baidu.com/jquery/1.9.1/jquery.min.js
 // @run-at      document-end
 // ==/UserScript==
-/*
-var script = document.createElement('script');
-script.src = 'http://libs.baidu.com/jquery/1.9.1/jquery.min.js';
-document.head.appendChild(script);
-*/
-var dateObj = new Date();
-if (!document.querySelector('#progress_counter_1')) {
-  var timeEnd;
-  if (localStorage.timeEnd) {
-    timeEnd = localStorage.timeEnd;
-  }
-  //console.log(timeEnd)
-  if (timeEnd === undefined || timeEnd <= dateObj.getTime()) {
-    $('div.clb').append('<div id="trainCountdown" style="font-weight:bold;font-size:large;"><a href="http://hentaiverse.org/?s=Character&ss=tr">训练已完成</a></div>');
-  } else {
-    var timeLast = parseInt((timeEnd - dateObj.getTime()) / 1000);
-    //console.log(timeLast)
-    $('div.clb').append('<div id="trainCountdown" style="font-weight:bold;font-size:large;">' + timeLast + '</div>');
+(function () {
+  var dateObj = new Date();
+  var countdownBox = document.createElement('div');
+  countdownBox.className = 'trainCountdown';
+  countdownBox.style.cssText = 'font-weight:bold;font-size:large;';
+  $('div.clb').appendChild(countdownBox);
+  if (!$('#progress_counter_1')) { //不位于训练界面或训练完成
+    var timeEnd = localStorage.timeEnd || '';
+    if (timeEnd === '' || timeEnd <= dateObj.getTime()) { //训练完成
+      $('.trainCountdown').innerHTML = '<a href="http://hentaiverse.org/?s=Character&ss=tr">训练已完成</a>';
+      document.title = '训练已完成';
+    } else { //不位于训练界面
+      var timeLast = parseInt((timeEnd - dateObj.getTime()) / 1000);
+      $('.trainCountdown').innerHTML = timeLast;
+      var timeDecrease = 0;
+      var timeUpdateIntarval = setInterval(timeUpdate, 1000);
+    }
+  } else { //位于训练界面且训练中
+    var nowTraining = $('#mainpane>div>table>tbody>tr>td>strong').innerText;
+    var nowTrainingProcess = $('#progress_counter_1').innerText;
+    var traningItems = document.querySelectorAll('#trainform tr>td:nth-child(1) .fd4>div');
+    for (var i = 0; i < traningItems.length; i++) {
+      if (traningItems[i].innerText === nowTraining) break;
+    }
+    var timeAll = parseInt($('#trainform tbody>tr:nth-child(' + (i + 2) + ')>td:nth-child(4) .fd4>div').innerText);
+    var timeLast = parseInt(timeAll * (1 - 0.01 * nowTrainingProcess) * 60 * 60);
+    localStorage.timeEnd = dateObj.getTime() + timeLast * 1000;
+    $('.trainCountdown').innerHTML = timeLast;
     var timeDecrease = 0;
-    var timeUpdateInterval = window.setInterval(timeUpdate, 1000);
+    var timeUpdateIntarval = setInterval(timeUpdate, 1000);
   }
-} else {
-  var nowTraining = $('#mainpane>div>table>tbody>tr>td>strong').text();
-  var nowTrainingProcess = $('#progress_counter_1').text();
-  var timeAll = parseInt($('#trainform>table>tbody>tr:contains(' + nowTraining + ')>td:nth-child(4)>div.fd4>div').text());
-  var timeLast = parseInt(timeAll * (1 - 0.01 * nowTrainingProcess) * 60 * 60);
-  localStorage.timeEnd = dateObj.getTime() + timeLast * 1000;
-  $('div.clb').append('<div id="trainCountdown" style="font-weight:bold;font-size:large;">' + timeLast + '</div>');
-  var timeDecrease = 0;
-  var timeUpdateInterval = window.setInterval(timeUpdate, 1000);
-}
-function timeUpdate() {
-  timeDecrease++;
-  if (timeLast <= timeDecrease) {
-    $('#trainCountdown').html('<a href="http://hentaiverse.org/?s=Character&ss=tr">训练已完成</a>')
-  } else {
-    var timeCountdownNow = timeLast - timeDecrease;
-    var second = Math.floor(timeCountdownNow % 60);
-    if (second < 10) second = '0' + second.toString();
-    var minite = Math.floor((timeCountdownNow / 60) % 60);
-    if (minite < 10) minite = '0' + minite.toString();
-    var hour = Math.floor((timeCountdownNow / 3600) % 24);
-    if (hour < 10) hour = '0' + hour.toString();
-    $('#trainCountdown').text(hour + ':' + minite + ':' + second);
+  function timeUpdate() {
+    timeDecrease++;
+    if (timeLast <= timeDecrease) {
+      $('.trainCountdown').innerHTML = '<a href="http://hentaiverse.org/?s=Character&ss=tr">训练已完成</a>';
+      document.title = '训练已完成';
+      clearInterval(timeUpdateIntarval);
+    } else {
+      var timeCountdownNow = timeLast - timeDecrease;
+      var second = Math.floor(timeCountdownNow % 60);
+      if (second < 10) second = '0' + second.toString();
+      var minite = Math.floor((timeCountdownNow / 60) % 60);
+      if (minite < 10) minite = '0' + minite.toString();
+      var hour = Math.floor((timeCountdownNow / 3600) % 24);
+      if (hour < 10) hour = '0' + hour.toString();
+      $('.trainCountdown').innerText = hour + ':' + minite + ':' + second;
+    }
   }
-}
+  function $(e) {
+    return document.querySelector(e);
+  }
+}) ();
