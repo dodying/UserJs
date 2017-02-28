8bd348875d1fd78be08945a936153f57f49ccf32
Dodying <dodying@users.noreply.github.com>
2017-02-28 20:59:33 +0800

    Create clear.user.js

---
+++ temp/clear.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         clear
+// @description  
+// @include      http*://hentaiverse.org/*
+// @exclude      http*://hentaiverse.org/pages/showequip.php?*
+// @author       Dodying
+// @namespace    https://github.com/dodying/UserJs
+// @supportURL   https://github.com/dodying/UserJs/issues
+// @icon         https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
+// @version      1
+// @run-at       document-end
+// ==/UserScript==
+
+localStorage.clear()
