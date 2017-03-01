03efcc9ffe98e9b669f57c3b5bf99b06a1d5b31a
Dodying <dodying@users.noreply.github.com>
2017-03-01 18:11:19 +0800

    Add files via upload

---
+++ temp/userAgent.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        userAgent
+// @name:zh-CN  
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @description:zh-CN  
+// @include     *
+// @version     1
+// @grant       none
+// @author      Dodying
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @supportURL  https://github.com/dodying/Dodying-UserJs/issues
+// @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
+// @run-at      document-end
+// ==/UserScript==
+(function () {
+  var d = document;
+  var t = d.createElement('table');
+  var h = '<tbody><tr><td>名</td><td>类型</td><td>值</td></tr>';
+  var n = navigator;
+  var i;
+  for (i in n) {
+    h += '<tr><td>' + i + '</td><td>' + (typeof n[i]) + '</td><td>' + n[i] + '</td></tr>';
+  }
+  h += '</tbody>';
+  t.innerHTML = h;
+  d.body.appendChild(t);
+}) ();
