6ab9598a85cf5412b5ca1cba9b09cec6ef0cff87
Dodying <dodying@users.noreply.github.com>
2017-03-11 11:57:17 +0800

    Add files via upload

---
+++ temp/1.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        AdBlocker-JS
+// @name:zh-CN  
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @description:zh-CN  
+// @include     http://m.d8qu.com/book/101891/
+// @version     1
+// @grant       unsafeWindow
+// @author      Dodying
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @supportURL  https://github.com/dodying/Dodying-UserJs/issues
+// @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
+// @run-at      document-start
+// @require http://cdn.staticfile.org/jquery/2.1.1/jquery.min.js
+// ==/UserScript==
+//const unsafeWindow.d8top = null;
+//const unsafeWindow.d8mid = null;
+//const unsafeWindow.d8bot = null;
+//$('article')
+//$('.intro').prev();$('div.footer').next();
+alert(1);
