96e0f4a4a25dee7f147b1f20025f6d1888beb952
Dodying <dodying@users.noreply.github.com>
2017-03-16 17:18:30 +0800

    Add files via upload

---
+++ temp/1.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        tryAudioBase64
+// @name:zh-CN  
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @description:zh-CN  
+// @include     https://iandevlin.com/html5/data-uri/audio.php
+// @version     1
+// @grant       none
+// @author      Dodying
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @supportURL  https://github.com/dodying/Dodying-UserJs/issues
+// @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
+// @run-at      document-end
+// ==/UserScript==
+
+document.querySelector('audio').play();
