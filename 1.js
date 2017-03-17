d1bcdea59c40b652523b568a87a3aaddc83d5633
Dodying <dodying@users.noreply.github.com>
2017-03-17 17:26:22 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        test-jQuery
+// @name:zh-CN  
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @description:zh-CN  
+// @include     *
+// @version     1
+// @grant       none
+// @author      Dodying
+// @namespace   https://github.com/dodying/Dodying-UserJs
+// @supportURL  https://github.com/dodying/Dodying-UserJs/issues
+// @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
+// @require     http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js
+// @run-at      document-end
+// ==/UserScript==
+
+alert($('body').text());
