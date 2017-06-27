9e5e39ce2763d1866a9630535ffbc03e0dc6edd9
Dodying <dodying@users.noreply.github.com>
2017-06-27 15:56:16 +0800

    v1.00
    以输入的答案为名称保存小马图片

---
+++ HentaiVerse/hvSaveImage.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        hvSaveImage
+// @author      Dodying
+// @include     http*://hentaiverse.org/*
+// @include     http*://alt.hentaiverse.org/*
+// @version     1.00
+// @grant       GM_xmlhttpRequest
+// @require     https://greasyfork.org/scripts/18532-filesaver/code/FileSaver.js?version=127839
+// @run-at      document-idle
+// ==/UserScript==
+(function () {
+  if (document.getElementById('riddlecounter')) {
+    var url = document.querySelector('#riddlebot>img').src;
+    GM_xmlhttpRequest({
+      method: 'GET',
+      url: url,
+      responseType: 'arraybuffer',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      onload: function (e) {
+        document.querySelector('#riddleanswer+img').addEventListener('click', function () {
+          saveAs(new Blob([e.response], {
+            type: 'image/jpeg'
+          }), document.getElementById('riddleanswer').value + '.jpg');
+        });
+      }
+    });
+  }
+}) ();
