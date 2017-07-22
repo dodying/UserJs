d78d3993d53baf71391f6accfc4df444144cd3e6
Dodying <dodying@users.noreply.github.com>
2017-07-22 16:54:05 +0800

    Update hvFeed.user.js

--- HentaiVerse/hvFeed.user.js
+++ HentaiVerse/hvFeed.user.js
@@ -1,45 +1,68 @@
 // ==UserScript==
-// @name        hvFeed
-// @name:zh-CN  【HV】喂食
-// @author      Dodying
-// @namespace   https://github.com/dodying/Dodying-UserJs
-// @supportURL  https://github.com/dodying/Dodying-UserJs/issues
-// @icon        http://cdn4.iconfinder.com/data/icons/mood-smiles/80/mood-29-48.png
-// @include     http*://alt.hentaiverse.org/?s=Bazaar&ss=ml
-// @include     http*://hentaiverse.org/?s=Bazaar&ss=ml
-// @version     1.01c
+// @name        【HV】怪物实验室-自动加点
+// @namespace   https://github.com/dodying/UserJs
+// @include     http://alt.hentaiverse.org/?s=Bazaar&ss=ml&slot=*
+// @version     1
 // @grant       none
-// @run-at      document-idle
+// @author      Dodying
+// @namespace   https://github.com/dodying/UserJs
+// @supportURL  https://github.com/dodying/UserJs/issues
+// @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
+// @run-at      document-end
 // ==/UserScript==
-function init() {
-  var items = document.querySelectorAll('.msa .fd4 div');
-  var moraleBar = document.querySelectorAll('div[style*="moralebar.png"]');
-  var level;
-  for (var i = 0; i < moraleBar.length; i++) {
-    level = items[5 * i + 2].innerHTML;
-    if (level >= 100 && !moraleBar[i].querySelector('img[src*="bar_green.png"]')) break;
-  }
-  feed(i + 1, moraleBar);
-}
-function feed(refer, moraleBar) {
-  var xhr;
-  xhr = new XMLHttpRequest();
-  xhr.open('POST', location + '&slot=' + refer);
-  var parm = 'food_action=happyhappyjoyjoy';
-  xhr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  xhr.responseType = 'document';
-  xhr.send(parm);
-  xhr.onload = function (e) {
-    var data = e.target.response;
-    if (!data.querySelector('div[style*="moralebar.png"]>img[src*="bar_green.png"]')) {
-      document.title = 'Happy Pill has been uesd up.';
-      return;
-    } else {
-      document.title = 'No.' + refer + ' Monster has been fed.';
-      moraleBar[refer - 1].parentNode.replaceChild(data.querySelector('div[style*="moralebar.png"]'), moraleBar[refer - 1]);
-      init();
+(function() {
+  var morale = document.querySelectorAll('.msl>div>div>img')[1].style.width;
+  if (morale !== '120px') {
+    var grades = [];
+    var elements = document.querySelectorAll('.mcr tr>td:nth-child(2)');
+    for (var i = 0; i < elements.length; i++) {
+      grades.push((document.querySelectorAll('.mcr tr')[i].querySelector('td:nth-child(1)>img[src*="_a.png"]')) ? parseInt(elements[i].textContent) : Infinity);
     }
+    var min = Math.min.apply(null, grades);
+    elements = document.querySelectorAll('.mcr tr>td:nth-child(1)>img');
+    var target;
+    for (i = 0; i < elements.length; i++) {
+      target = elements[inArray(min, grades)];
+      if (target.hasAttribute('onclick')) {
+        target.click();
+        break;
+      } else {
+        grades[inArray(min, grades)] = -1;
+      }
+    }
+  } else {
+    if (document.querySelector('[src="/y/character/inc.png"]')) {
+      var img = document.querySelectorAll('[src="/y/character/inc.png"]');
+      for (var i = 0; i < img.length; i++) {
+        var temp = img[i].getAttribute('onmouseover').match(/Upgrade Cost: (\d+) Chaos Tokens?[ ]+Stock: (\d+)/);
+        if (parseInt(temp[1]) <= parseInt(temp[2])) {
+          img[i].click();
+          return;
+        }
+      }
+    }
+    var slot = location.href.match(/slot=(\d+)/)[1];
+    var dateNow = new Date();
+    dateNow = dateNow.getUTCFullYear() + '/' + (dateNow.getUTCMonth() + 1) + '/' + dateNow.getUTCDate();
+    var monsterLab = (localStorage.monsterLab) ? JSON.parse(localStorage.monsterLab) : {
+      date: dateNow
+    };
+    if (monsterLab.date === dateNow && monsterLab[slot] === true) {
+      return;
+    } else if (monsterLab.date !== dateNow) {
+      monsterLab = {
+        date: dateNow
+      };
+    }
+    monsterLab[slot] = true;
+    localStorage.monsterLab = JSON.stringify(monsterLab);
+    document.querySelector('#monster_nav>div:nth-child(3)>img').click();
   }
-}
-init();
+
+  function inArray(text, array) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] === text) return i;
+    }
+    return false;
+  }
+})();
