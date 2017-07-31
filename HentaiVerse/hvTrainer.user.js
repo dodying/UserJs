858a939d200600280bb28cd14ddd40385ba4599d
Dodying <dodying@users.noreply.github.com>
2017-07-31 17:57:34 +0800

    v1.02
    修改自动训练的设置问题

--- HentaiVerse/hvTrainer.user.js
+++ HentaiVerse/hvTrainer.user.js
@@ -7,14 +7,14 @@
 // @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
 // @include     http*://hentaiverse.org/*
 // @include     http*://alt.hentaiverse.org/*
-// @version     1.01d
-// @grant       none
+// @version     1.02
+// @grant       GM_registerMenuCommand
 // @run-at      document-end
 // ==/UserScript==
 (function() {
   if (!gE('#navbar')) return;
-  var trainItem = 'Adept Learner';
-  var countdownBox = gE('body').appendChild(cE('div'));
+  var countdownBox = gE('body').appendChild(cE('a'));
+  countdownBox.href = '?s=Character&ss=tr';
   countdownBox.style.cssText = 'font-weight:bold;font-size:large;position:absolute;top:2px;right:2px';
   var timeLast;
   var trainList = {
@@ -79,16 +79,139 @@
       'time': 12
     }
   };
+  var trainList2 = {
+    '50': 1,
+    '51': 24,
+    '70': 4,
+    '71': 8,
+    '72': 12,
+    '80': 2,
+    '81': 24,
+    '84': 24,
+    '85': 24,
+    '88': 24,
+    '90': 0,
+    '91': 0,
+    '96': 12,
+    '98': 0,
+    '': 24
+  };
+  var lang = navigator.language;
+  var timeOption = { hour12: false };
+  GM_registerMenuCommand('Train List', function() {
+    var time = countdownBox.value || new Date().getTime();
+    var trainTask;
+    var trainWindow = window.open('', 'trainWindow', 'resizable,scrollbars,width=800,height=450');
+    var doc = trainWindow.document;
+    var style = gE('head', doc).appendChild(cE('style'));
+    style.textContent = '*{margin:5px;text-align:center;}table{border:2px solid #000;border-collapse:collapse;margin:0 auto;}table>tbody>tr>td{border:1px solid #000;}input{text-align:right;width:60px;}';
+    var table = gE('body', doc).appendChild(cE('table'));
+    var tbody = table.appendChild(cE('tbody'));
+    var tr = tbody.appendChild(cE('tr'));
+    tr.innerHTML = '<td></td><td>Project</td><td>Freq</td><td>Start Time - End Time</td>';
+    var select = [
+      '<select>',
+      '<option value="-1"></option>',
+      '<option value="50">Adept Learner</option>',
+      '<option value="51">Assimilator</option>',
+      '<option value="80">Ability Boost</option>',
+      '<option value="81">Manifest Destiny</option>',
+      '<option value="70">Scavenger</option>',
+      '<option value="71">Luck of the Draw</option>',
+      '<option value="72">Quartermaster</option>',
+      '<option value="">Archaeologist</option>',
+      '<option value="84">Metabolism</option>',
+      '<option value="85">Inspiration</option>',
+      '<option value="90">Scholar of War</option>',
+      '<option value="91">Tincture</option>',
+      '<option value="98">Pack Rat</option>',
+      '<option value="88">Dissociation</option>',
+      '<option value="96">Set Collector</option>',
+      '</select>',
+    ].join('');
+    var order = 1;
+    var i, _time, _select, _input;
+    var buttonNew = gE('body', doc).appendChild(cE('button'));
+    buttonNew.textContent = 'New Task';
+    buttonNew.onclick = function() {
+      tr = tbody.appendChild(cE('tr'));
+      tr.innerHTML = '<td>' + (order++) + '</td><td>' + select + '</td><td><input type="number" placeholder="1" min="1"></td><td></td>';
+      gE('select', tr).value = '-1';
+    };
+    var buttonSave = gE('body', doc).appendChild(cE('button'));
+    buttonSave.textContent = 'Save Task';
+    buttonSave.onclick = function() {
+      var input = gE('select,input', 'all', tbody);
+      trainTask = [];
+      for (i = 0; i < input.length; i = i + 2) {
+        if (input[i].value !== '-1') trainTask.push({
+          id: input[i].value,
+          freq: (input[i + 1].value || input[i + 1].placeholder) * 1
+        });
+      }
+      setValue('trainTask', trainTask);
+      trainWindow.close();
+      location.href = location.href;
+    };
+    if (getValue('trainTask') && getValue('trainTask') !== '[]') {
+      trainTask = getValue('trainTask', true);
+      for (i = 0; i < trainTask.length; i++) {
+        tr = tbody.appendChild(cE('tr'));
+        tr.innerHTML = '<td>' + (order++) + '</td><td>' + select + '</td><td><input type="number" value="' + trainTask[i].freq + '" placeholder="1" min="1"></td><td></td>';
+        gE('select', tr).value = trainTask[i].id;
+      }
+      timeChange();
+    } else {
+      buttonNew.click();
+    }
+    tbody.onclick = changeEvent;
+    tbody.onkeyup = changeEvent;
+    trainWindow.document.close();
+
+    function timeChange() {
+      _time = gE('tr>td:nth-child(4)', 'all', tbody);
+      _select = gE('select', 'all', tbody);
+      _input = gE('input', 'all', tbody);
+      for (i = 0; i < _select.length; i++) {
+        _time[i + 1].textContent = _select[i].value === '-1' ? '' : timeStr(_input[i].value * 1 * trainList2[_select[i].value]);
+      }
+    }
+
+    function timeStr(hour) {
+      var start = start || time;
+      time = start + hour * 60 * 60 * 1000;
+      return new Date(start).toLocaleString(lang, timeOption) + ' - ' + new Date(time).toLocaleString(lang, timeOption);
+    }
+
+    function changeEvent(e) {
+      if (e.target.tagName !== 'SELECT' && e.target.tagName !== 'INPUT') return;
+      time = countdownBox.value || new Date().getTime();
+      timeChange();
+    }
+  }, 'T');
   post('?s=Character&ss=tr', function(data) {
     if (gE('#train_progcnt', data)) {
       var nowTraining = gE('#train_progress>div>strong', data).innerText;
       var nowTrainingProcess = gE('#train_progcnt', data).innerText;
       var timeAll = trainList[nowTraining].time;
       timeLast = parseInt(timeAll * (1 - 0.01 * nowTrainingProcess) * 60 * 60);
+      var timeEnd = new Date(new Date().getTime() + timeLast * 1000);
+      countdownBox.title = timeEnd.toLocaleString(lang, timeOption);
+      countdownBox.value = timeEnd.getTime();
       timeUpdate();
     } else {
-      if (trainItem !== '') autoTrain(trainList[trainItem]);
-      countdownBox.innerHTML = '<a href="/?s=Character&ss=tr">Train Completed</a>';
+      if (getValue('trainTask') && getValue('trainTask') !== '[]') {
+        var trainTask = getValue('trainTask', true);
+        if (trainTask[0].freq <= 0) trainTask.splice(0, 1);
+        if (trainTask.length > 0) {
+          trainTask[0].freq--;
+          post('?s=Character&ss=tr', function() {
+            location.href = location.href;
+          }, 'start_train=' + trainTask[0].id);
+        }
+        setValue('trainTask', trainTask);
+      }
+      countdownBox.innerHTML = 'Train Completed';
       document.title = 'Train Completed';
     }
   });
@@ -113,53 +236,54 @@
       location.href = location.href;
     }, 1000 * 60 * 10);
   }
-
-
-  function autoTrain(item) {
-    post('?s=Character&ss=tr', function() {
-      location.href = location.href;
-    }, 'start_train=' + item.id);
-  }
-
-  function gE(ele, mode, parent) { //获取元素
-    if (typeof ele === 'object') {
-      return ele;
-    } else if (mode === undefined && parent === undefined) {
-      return (isNaN(ele * 1)) ? document.querySelector(ele) : document.getElementById(ele);
-    } else if (mode === 'all') {
-      return (parent === undefined) ? document.querySelectorAll(ele) : parent.querySelectorAll(ele);
-    } else if (typeof mode === 'object' && parent === undefined) {
-      return mode.querySelector(ele);
-    }
-  }
-
-  function cE(name) { //创建元素
-    return document.createElement(name);
-  }
-
-  function post(href, func, parm) { //post
-    var xhr = new XMLHttpRequest();
-    xhr.open(parm ? 'POST' : 'GET', href);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    xhr.responseType = 'document';
-    xhr.onerror = function() {
-      xhr = null;
-      post(href, func, parm);
-    };
-    xhr.onload = function(e) {
-      if (e.target.status >= 200 && e.target.status < 400 && typeof func === 'function') {
-        var data = e.target.response;
-        if (xhr.responseType === 'document' && gE('#messagebox', data)) {
-          if (gE('#messagebox')) {
-            gE('#csp').replaceChild(gE('#messagebox', data), gE('#messagebox'));
-          } else {
-            gE('#csp').appendChild(gE('#messagebox', data));
-          }
-        }
-        func(data, e);
-      }
-      xhr = null;
-    };
-    xhr.send(parm);
-  }
 })();
+
+function setValue(item, value) {
+  localStorage[item] = (typeof value === 'string') ? value : JSON.stringify(value);
+}
+
+function getValue(item, toJSON) {
+  return (localStorage[item]) ? ((toJSON) ? JSON.parse(localStorage[item]) : localStorage[item]) : null;
+}
+
+function gE(ele, mode, parent) { //获取元素
+  if (typeof ele === 'object') {
+    return ele;
+  } else if (mode === undefined && parent === undefined) {
+    return (isNaN(ele * 1)) ? document.querySelector(ele) : document.getElementById(ele);
+  } else if (mode === 'all') {
+    return (parent === undefined) ? document.querySelectorAll(ele) : parent.querySelectorAll(ele);
+  } else if (typeof mode === 'object' && parent === undefined) {
+    return mode.querySelector(ele);
+  }
+}
+
+function cE(name) { //创建元素
+  return document.createElement(name);
+}
+
+function post(href, func, parm) { //post
+  var xhr = new XMLHttpRequest();
+  xhr.open(parm ? 'POST' : 'GET', href);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+  xhr.responseType = 'document';
+  xhr.onerror = function() {
+    xhr = null;
+    post(href, func, parm);
+  };
+  xhr.onload = function(e) {
+    if (e.target.status >= 200 && e.target.status < 400 && typeof func === 'function') {
+      var data = e.target.response;
+      if (xhr.responseType === 'document' && gE('#messagebox', data)) {
+        if (gE('#messagebox')) {
+          gE('#csp').replaceChild(gE('#messagebox', data), gE('#messagebox'));
+        } else {
+          gE('#csp').appendChild(gE('#messagebox', data));
+        }
+      }
+      func(data, e);
+    }
+    xhr = null;
+  };
+  xhr.send(parm);
+}
