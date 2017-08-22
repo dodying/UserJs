b2670a8f36be4995bf54fe7a8a9736a8d8923eae
Dodying <dodying@users.noreply.github.com>
2017-08-22 22:33:27 +0800

    v.031a

--- novel/novelDownloader/novelDownloaderHelper.user.js
+++ novel/novelDownloader/novelDownloaderHelper.user.js
@@ -2,11 +2,11 @@
 // @name        novelDownloaderHelper
 // @namespace   https://github.com/dodying/Dodying-UserJs
 // @name:zh-CN  【小说】下载脚本助手
-// @description:zh-CN  
+// @description:zh-CN
 // @include     *
 // @exclude     *.baidu.com*
 // @exclude     *.google.*
-// @version     1.031
+// @version     1.031a
 // @grant       GM_setClipboard
 // @author      Dodying
 // @namespace   https://github.com/dodying/Dodying-UserJs
@@ -14,39 +14,30 @@
 // @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
 // @run-at      document-end
 // ==/UserScript==
-var info;
-var pretitle = document.title;
-window.onkeydown = function (e) {
-  if (e.altKey || e.ctrlKey || e.metaKey || e.keyCode === 13) return
-  info = '';
-  if (e.shiftKey && e.keyCode === 72) { //H
-    var gbk = (document.querySelector('meta[content*="charset=gb"],meta[charset*="gb"]')) ? ',0,1' : '';
-    var text = '\n'
-    + 'addIRule(\'' + location.host + '\',\'' + pretitle.replace(/.*\- /, '') + '\',\'\',\'\');\n'
-    + 'addCRule(\'' + location.host + '\',\'\',\'\'' + gbk + ');';
+window.onkeydown = function(e) {
+  if (e.target.tagName === 'INPUT' || e.target.tagName === 'TEXTAREA') return;
+  if (e.altKey || e.ctrlKey || e.metaKey || e.keyCode === 13) return;
+  var info = '';
+  var pretitle = document.title;
+  var text = '';
+  if (e.shiftKey && e.keyCode === 72) { //Shift+H
+    var h1 = document.querySelectorAll('h1').length === 1 ? 'h1' : '';
+    var gbk = (document.querySelector('meta[content*="charset=gb"],meta[charset*="gb"],script[charset*="gb"]')) ? ',1' : '';
+    text = '\n' +
+      'addIRule(\'' + location.host + '\',\'' + pretitle.replace(/.*(,|-)/, '').trim() + '\',\'' + h1 + '\',\'\');\n' +
+      'addCRule(\'' + location.host + '\',\'\',\'\'' + gbk + ');';
     info = 'index';
-  } else if (e.shiftKey && e.keyCode === 85) { //U
-    var text = '\n'
-    + '// @include     ' + location.href;
+  } else if (e.shiftKey && e.keyCode === 85) { //Shift+U
+    text = '\n' + '// @include     ' + location.href;
     info = 'include';
-  } else if (e.shiftKey && e.keyCode === 82) { //R
-    var text = 'addRRule(\'' + location.host + '\', \'\\\\s+||| \', \'\');';
+  } else if (e.shiftKey && e.keyCode === 82) { //Shift+R
+    text = 'addRRule(\'' + location.host + '\', \'\\\\s+||| \', \'\');';
     info = 're';
-  } else if (e.shiftKey && e.keyCode === 65) { //A
-    var gbk = (document.querySelector('meta[content*="charset=gb"]')) ? ',0,1' : '';
-    var space = '    ';
-    var text = '\n'
-    + space + '"' + location.host + '": {\n'
-    + space + space + '"include1": "' + location.href + '",\n'
-    + space + space + '"indexRule": "' + 'addIRule(\'' + location.host + '\',\'' + pretitle.replace(/.*\- /, '') + '\',\'\',\'\');' + '",\n'
-    + space + space + '"chapterRule": "' + 'addCRule(\'' + location.host + '\',\'\',\'\'' + gbk + ');' + '"\n'
-    + space + '},';
-    info = 'all';
   }
   if (info !== '') {
     GM_setClipboard(text);
     document.title = '[已复制' + info + ']' + pretitle;
-    setTimeout(function () {
+    setTimeout(function() {
       document.title = pretitle;
     }, 3000);
   }
