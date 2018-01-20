acb10b4c8f482e0abad6d0de500e3b5dd46006c0
dodying <549779963@qq.com>
2018-01-20 17:29:07 +0800

    v2.88.4

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -5,7 +5,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.88.3
+// @version      2.88.4
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -1375,7 +1375,7 @@ function checkCondition(parms) {
 }
 //答题//
 function riddleAlert() { //答题警报
-  gE('#riddleanswer+img').onclick = function (){
+  if (window.opener) gE('#riddleanswer+img').onclick = function (){
     riddleSubmit(gE('#riddleanswer').value);
   };
   setAlarm('Riddle');
