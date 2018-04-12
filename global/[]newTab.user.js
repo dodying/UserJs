12a30828de503f08b806e8c699e8ccac266b0067
dodying <549779963@qq.com>
2018-04-12 21:25:20 +0800

    v0.2.0.0+1

--- global/[]newTab.user.js
+++ global/[]newTab.user.js
@@ -3,20 +3,49 @@
 // @author      daysv dodying
 // @namespace   http://daysv.github.com
 // @description 链接强制在新建标签中打开 Open a URL in a new tab
-// @version     0.2.0.0
+// @version     0.2.0.0+1
 // @include     http*://*/*
 // @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
+// @grant       GM_registerMenuCommand
+// @grant       GM_unregisterMenuCommand
+// @grant       GM_getValue
+// @grant       GM_setValue
 // @run-at      document-end
 // ==/UserScript==
-(function() {
+(function total() {
   /*
-  var observer = new MutationObserver(init);
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
+    var observer = new MutationObserver(init);
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
   */
-  init();
+  let blacklist = GM_getValue('blacklist', []);
+  let host = location.host;
+  if (blacklist.includes(host)) {
+    let id;
+    id = GM_registerMenuCommand('newTab: Effect ' + host, function() {
+      let blacklist = GM_getValue('blacklist', []);
+      if (blacklist.includes(host)) {
+        blacklist.splice(blacklist.indexOf(host), 1);
+        GM_setValue('blacklist', blacklist);
+        GM_unregisterMenuCommand(id);
+        total();
+      }
+    }, 'N');
+  } else {
+    init();
+    let id;
+    id = GM_registerMenuCommand('newTab: DO NOT Effect ' + host, function() {
+      let blacklist = GM_getValue('blacklist', []);
+      if (!blacklist.includes(host)) {
+        blacklist.push(host);
+        GM_setValue('blacklist', blacklist);
+        GM_unregisterMenuCommand(id);
+        total();
+      }
+    }, 'N');
+  }
 
   function init() {
     var raw = [];
@@ -47,6 +76,6 @@
         link.setAttribute('target', '_blank');
       }
     });
-    console.error('Set newtab: ', changed, '\ndon\'t change: ', raw);
+    /*console.error('Set newtab: ', changed, '\ndon\'t change: ', raw);*/
   }
 })();
