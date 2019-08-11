d65c992e5d89f783dd6dce0e7b9965512635aed0
dodying <dodying@qq.com>
2019-08-11 17:30:45 +0800

    v1.0.37

--- global/prettyJSON.user.js
+++ global/prettyJSON.user.js
@@ -2,8 +2,8 @@
 // @name        prettyJSON
 // @description prettyJSON
 // @include     *
-// @version     1.0.7
-// @date        2019-2-8 18:36:21
+// @version     1.0.37
+// @modified    2019-8-6 13:30:18
 // @author      dodying
 // @namespace   https://github.com/dodying/UserJs
 // @supportURL  https://github.com/dodying/UserJs/issues
@@ -14,18 +14,23 @@
 // ==/UserScript==
 (function () {
   'use strict'
+
   try {
     let json = JSON.parse(document.body.textContent)
-    document.body.innerHTML = '<pre>' + htmlEscape(JSON.stringify(json, null, 2)) + '</pre>'
     console.log(json)
-  } catch (error) {
+    if (JSON.stringify(json) === '{}') return
+    let html = htmlEscape(JSON.stringify(json, null, 2))
+    html = html.replace(/(https?:\/\/.*?)(\s|")/g, '<a href="$1" target="_blank">$1</a>$2')
+    if (typeof json === 'object') document.body.innerHTML = '<pre>' + html + '</pre>'
+  } catch (error) {}
 
-  }
   function htmlEscape (text) {
     return text.replace(/["&<>]/g, function (match) {
       return {
-        '"': '&quot;',
-        '&': '&amp;',
+        // '"': '&quot;',
+        // '&': '&amp;',
+        '"': '"',
+        '&': '&',
         '<': '&lt;',
         '>': '&gt;'
       }[match]
