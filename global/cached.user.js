bfbdc4ea4e014fea0ed37800f1ff86157f8d3b99
dodying <549779963@qq.com>
2020-02-25 10:29:52 +0800

    upload new

---
+++ global/cached.user.js
@@ -0,0 +1,111 @@
+// ==UserScript==
+// @name        []cached
+// @description cached
+// @include     http://*
+// @include     https://*
+// @version     1.0.109
+// @created     2019-11-24 18:48:14
+// @modified    2019-11-25 09:31:55
+// @author      dodying
+// @namespace   https://github.com/dodying/UserJs
+// @supportURL  https://github.com/dodying/UserJs/issues
+// @icon        https://raw.githubusercontent.com/dodying/UserJs/master/Logo.png
+// @run-at      document-end
+// @grant       GM_xmlhttpRequest
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_notification
+// @grant       GM_openInTab
+// @grant       GM_registerMenuCommand
+// @noframes
+// ==/UserScript==
+/* eslint-disable no-debugger  */
+'use strict'
+;(async function () {
+  let blacklist = GM_getValue('blacklist', [])
+  let checkedlist = GM_getValue('checkedlist', [])
+  let host = window.location.host
+  let url = `https://2tool.top/kuaizhao.php?k=${encodeURIComponent(window.location.href)}`
+
+  if (blacklist.includes(host)) {
+    GM_registerMenuCommand('Cached: Effect ' + host, function () {
+      let blacklist = GM_getValue('blacklist', [])
+      if (blacklist.includes(host)) {
+        blacklist.splice(blacklist.indexOf(host), 1)
+        GM_setValue('blacklist', blacklist)
+        window.location.reload()
+      }
+    })
+    return
+  }
+
+  GM_registerMenuCommand('Cached: DO NOT Effect ' + host, function () {
+    let blacklist = GM_getValue('blacklist', [])
+    if (!blacklist.includes(host)) {
+      blacklist.push(host)
+      GM_setValue('blacklist', blacklist)
+      window.location.reload()
+    }
+  })
+
+  if (checkedlist.includes(host)) {
+    GM_registerMenuCommand('Cached: Show cached', function () {
+      GM_openInTab(url, true)
+    })
+    return
+  }
+
+  let body = await xhrSync(url)
+  let args = body.response.match(/doLoadKz\(('.*)\)/g).map(i => i.match(/doLoadKz\(('.*)\)/)[1]).map(i => i.split(',').map(j => j.replace(/^['"](.*)['"]$/, '$1')))
+  await Promise.all(args.map(arg => doLoadKz(...arg))).then(result => {
+    let caches = result.map(i => i.response.match(/^callback\d\((.*)\)$/)[1]).map(i => JSON.parse(i)).map(i => i.kzUrl).filter(i => i)
+    if (caches.length) {
+      let checkedlist = GM_getValue('checkedlist', [])
+      checkedlist.push(host)
+      GM_setValue('checkedlist', checkedlist)
+    }
+    if (!caches.length) {
+      GM_notification({
+        text: caches.length ? '已有快照' : '不存在快照',
+        title: document.title,
+        image: window.location.origin + '/favicon.ico',
+        tag: GM_info.script.name,
+        timeout: 10 * 1000,
+        onclick: function () {
+          GM_openInTab(caches.length ? url : `https://web.archive.org/save/${window.location.href}`, true)
+        }
+      })
+    }
+  })
+})()
+
+async function doLoadKz (s, typeId, num) {
+  var preUrl = 'https://2tool.top'
+  var url = preUrl + '/kz.php?s=' + s + '&num=' + num
+  return xhrSync(url)
+}
+
+function xhrSync (url, parm = null, opt = {}) {
+  console.log({ url, parm })
+  return new Promise((resolve, reject) => {
+    GM_xmlhttpRequest({
+      method: parm ? 'POST' : 'GET',
+      url: url,
+      data: parm,
+      timeout: opt.timeout || 60 * 1000,
+      responseType: ['arraybuffer', 'blob', 'json'].includes(opt.responseType) ? opt.responseType : null,
+      headers: opt.headers || {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      onload (res) {
+        resolve(res)
+      },
+      ontimeout (res) {
+        reject(res)
+      },
+      onerror (res) {
+        reject(res)
+      }
+    })
+  })
+}
