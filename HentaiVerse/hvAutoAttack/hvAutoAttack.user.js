ebbe8d8dacddd54beefd4efd4a0614273db7dee6
dodying <549779963@qq.com>
2020-05-31 09:01:49 +0800

    v2.90.2
    fix [#58](issues/58), thanks to sddiky

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -5,7 +5,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.1
+// @version      2.90.2
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -972,12 +972,6 @@ function optionBox () { // 配置界面
     if (_alert(1, '是否重置', '是否重置', 'Whether to reset')) {
       setValue('option', option)
       goto()
-    }
-  }
-  // 标签页-反馈
-  gE('.joinQun', optionBox).onclick = function (e) {
-    if (!window.confirm() || !window.confirm() || !window.confirm()) {
-      e.preventDefault()
     }
   }
   //
