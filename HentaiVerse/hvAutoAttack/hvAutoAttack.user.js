5a487fcb7b4a9da18a1577b5c9228aac3f08a899
WayneFerdon <wayneferdon@hotmail.com>
2025-06-14 16:53:03 +0800

    增加严格模式下的兼容

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.22.9
+// @version      2.90.22.10
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -116,9 +116,11 @@ try {
     console.log(`${state ? 'Start' : 'End'} ${name}\n`, JSON.parse(JSON.stringify(asyncList)));
   }
   function logSwitchAsyncTask(args) {
-    const argsStr = Array.from(args).join(',');
-    const name = `${args.callee.name}${argsStr === '' ? argsStr : `(${argsStr})`}`;
-    consoleAsyncTasks(name, asyncList.indexOf(name) === -1);
+    try{
+      const argsStr = Array.from(args).join(',');
+      const name = `${args.callee.name}${argsStr === '' ? argsStr : `(${argsStr})`}`;
+      consoleAsyncTasks(name, asyncList.indexOf(name) === -1);
+    }catch(e){}
   }
 
   //ajax
@@ -1574,7 +1576,7 @@ try {
         }, 0.5 * _1s);
         return;
       }
-      const arenaPrev = g('option')?.idleArenaValue;
+      const arenaPrev = g('option').idleArenaValue;
       const _option = {
         version: g('version'),
       };
